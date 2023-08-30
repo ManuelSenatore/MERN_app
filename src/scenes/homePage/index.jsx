f4835bac219addbeb248054fe7336e3182d17927
m.senatore <m.senatore@jdk.it>
2023-08-30 14:24:21 +0200

    login page

---
+++ src/scenes/homePage/index.jsx
@@ -0,0 +1,13 @@
+import { Box } from '@mui/material'
+import React from 'react'
+import NavBar from 'scenes/navbar'
+
+const HomePage = () => {
+  return (
+    <Box>
+      <NavBar/>
+    </Box>
+  )
+}
+
+export default HomePage
